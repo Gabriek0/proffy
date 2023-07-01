770f66404a9a379922a99137490e120ed7b73fbd
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2023-07-01 17:17:13 -0300

    feat: add list users endpoint

--- packages/backend/src/app/app.controller.ts
+++ packages/backend/src/app/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { DatabaseService } from '../database/database.service';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly db: DatabaseService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('users')
+  listUsers() {
+    return this.db.user.findMany()
   }
 }
