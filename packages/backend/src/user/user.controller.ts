ca071e393a4afc5fd033e4e480969fa2ad63b707
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-15 13:48:31 -0300

    feat: create post method in user.controller

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   // Dependency injection
-  constructor();
+  constructor(private readonly UserService: UserService) {}
 
-
+  @Post('/create')
+  createUser(@Body() user: CreateUserDTO) {
+    return this.UserService.createUser(user);
+  }
 }
