2360ddcd4bf9a36cb0ddc8ccd64054a30d2af0da
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-15 14:35:52 -0300

    feat: add UserModule to app.module.ts and import DatabaseModule to user.module.ts

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
-  imports: [UserService],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
