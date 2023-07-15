761049cbc65b1bedbd7b3f717a89f184b7414b78
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-15 13:46:11 -0300

    feat: add createUser method in user.service

---
+++ packages/backend/src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+import { DatabaseService } from '../database/database.service';
+
+import { CreateUserDTO } from './dto/create-user.dto';
+
+import bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: DatabaseService) {}
+
+  async createUser(user: CreateUserDTO): Promise<User> {
+    const passwordHash = await bcrypt.hash(user.password, 10);
+
+    const user_created = await this.prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        passwordHash: passwordHash,
+      },
+    });
+
+    return user_created;
+  }
+}
