d0c1b3f8fefb323de7f1d6da4d36fa2896da141c
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-18 17:52:45 -0300

    feat: add config parameters for @ApiProperty decorator

--- packages/backend/src/user/dto/create-user.dto.ts
+++ packages/backend/src/user/dto/create-user.dto.ts
@@ -11,11 +11,19 @@ import { User } from '../entities/user.entity';
 
 export class CreateUserDTO extends User {
   @IsString()
-  @ApiProperty()
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Username',
+    required: true,
+  })
   name: string;
 
   @IsEmail()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User email',
+    example: 'example@example.com',
+    required: true,
+  })
   email: string;
 
   @IsString()
@@ -24,6 +32,12 @@ export class CreateUserDTO extends User {
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'Your password is too weak',
   })
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User password',
+    example: 'Ex4mple_strong_password@',
+    minimum: 4,
+    maximum: 20,
+    required: true,
+  })
   password: string;
 }
