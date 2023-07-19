65ae137f4a683c928f904295744a6fe66ad65b7d
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-19 09:17:32 -0300

    feat: change @ApiProperty decorator order

--- packages/backend/src/user/dto/create-user.dto.ts
+++ packages/backend/src/user/dto/create-user.dto.ts
@@ -10,34 +10,34 @@ import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDTO extends User {
-  @IsString()
   @ApiProperty({
     example: 'John Doe',
     description: 'Username',
     required: true,
   })
+  @IsString()
   name: string;
 
-  @IsEmail()
   @ApiProperty({
     description: 'User email',
     example: 'example@example.com',
     required: true,
   })
+  @IsEmail()
   email: string;
 
-  @IsString()
-  @MinLength(4)
-  @MaxLength(20)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Your password is too weak',
-  })
   @ApiProperty({
     description: 'User password',
     example: 'Password@_exampl3',
     minimum: 4,
     maximum: 20,
     required: true,
+  })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Your password is too weak',
   })
   password: string;
 }
