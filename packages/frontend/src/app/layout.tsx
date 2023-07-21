0f3da3a9c81016c6f1c69fb0f91011641a6b85b4
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-20 21:11:46 -0300

    feat: configure next fonts

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Poppins } from 'next/font/google';
 import './global.css';
 
 export const metadata = {
@@ -5,13 +6,20 @@ export const metadata = {
   description: 'Sua plataforma de estudos online',
 };
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  style: 'normal',
+  display: 'swap',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>{children}</body>
     </html>
   );
