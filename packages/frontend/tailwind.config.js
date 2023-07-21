c87244f8fedea52061a6b2d33fe5b1443d58732f
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-20 21:02:32 -0300

    feat: create colors pattern

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        green: '#04D361',
+        background: '#F0F0F7',
+        gray: {
+          10: '#FAFAFC',
+          20: '#F0F0F7',
+          30: '#E6E6F0',
+          40: '#9C98A6',
+        },
+        purple: {
+          100: '#D4C2FF',
+          200: '#9871F5',
+          300: '#8257E5',
+          500: '#6A6180',
+          600: '#32264D',
+        },
+      },
+    },
   },
   plugins: [],
 };
