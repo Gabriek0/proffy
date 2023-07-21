c1e83a24239e0155bd70a593be51c4d78863d143
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-07-20 21:16:54 -0300

    feat: add background

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.className}>
-      <body>{children}</body>
+      <body className="bg-background">{children}</body>
     </html>
   );
 }
