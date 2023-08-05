5034c2f1c9eb749da500339c2e52922631a7b5c5
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-05 19:39:06 -0300

    feat: add width to sections

--- packages/frontend/src/app/create-account/page.tsx
+++ packages/frontend/src/app/create-account/page.tsx
@@ -3,17 +3,17 @@ import { Input } from '../../components/Input';
 
 export default async function CreateAccountPage() {
   return (
-    <main className="h-screen w-screen flex items-center ">
-      <section className="flex flex-col gap-[1rem] p-[2rem]">
-        <h1 className="text-purple-600 font-semibold leading-[34px] text-[2.25rem] ">
-          Cadastro
-        </h1>
+    <main className="h-screen w-screen flex items-center">
+      <section className="flex justify-center items-center p-[2rem] h-full w-1/2">
+        <form className="w-[22rem] mt-[2rem]">
+          <h1 className="text-purple-600 font-semibold leading-[34px] text-[2.25rem] mb-[1rem]">
+            Cadastro
+          </h1>
 
-        <h2 className=" text-purple-500 font-normal text-[1.25rem] leading-[26px]">
-          Preencha os dados abaixo <br /> para começar.
-        </h2>
+          <h2 className=" text-purple-500 font-normal text-[1.25rem] leading-[26px] mb-[2.5rem] ">
+            Preencha os dados abaixo <br /> para começar.
+          </h2>
 
-        <form action="" className="mt-[2rem]">
           <Input.Root variantStyle="topRounded">
             <Input.Content label="Nome" name="first_name" />
           </Input.Root>
@@ -34,9 +34,7 @@ export default async function CreateAccountPage() {
         </form>
       </section>
 
-      <section className="bg-purple-300">
-        <h1>Proffy</h1>
-      </section>
+      <section className="h-full w-[50%] bg-purple-300 "></section>
     </main>
   );
 }
