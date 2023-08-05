532da7d2b219debe3858df7251cf4a589087042b
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-05 18:12:57 -0300

    feat: create create-account screen initial structure

--- packages/frontend/src/app/create-account/page.tsx
+++ packages/frontend/src/app/create-account/page.tsx
@@ -2,10 +2,40 @@ import { Input } from '../../components/Input';
 
 export default async function CreateAccountPage() {
   return (
-    <>
-      <Input.Root>
-        <Input.Content label="Nome" name="name" />
-      </Input.Root>
-    </>
+    <main className="h-screen w-screen flex items-center ">
+      <section className="flex flex-col gap-[1rem] p-[2rem]">
+        <h1 className="text-purple-600 font-semibold leading-[34px] text-[2.25rem] ">
+          Cadastro
+        </h1>
+
+        <h2 className=" text-purple-500 font-normal text-[1.25rem] leading-[26px]">
+          Preencha os dados abaixo <br /> para começar.
+        </h2>
+
+        <form action="" className="mt-[2rem]">
+          <Input.Root>
+            <Input.Content label="Nome" name="first_name" />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Content label="Sobrenome" name="last_name" />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Content label="E-mail" name="email" />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Content label="Senha" name="password" />
+          </Input.Root>
+
+          <button>Concluir Cadastro</button>
+        </form>
+      </section>
+
+      <section className="bg-purple-300">
+        <h1>Proffy</h1>
+      </section>
+    </main>
   );
 }
