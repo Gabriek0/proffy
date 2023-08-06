f495f211c1fa6f9185ff54ff65e4d456d7f7d2a5
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2023-08-06 16:55:55 -0300

    feat(backend): initial class service

--- packages/backend/src/classes/class.service.ts
+++ packages/backend/src/classes/class.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
+import { CreateClassDto } from './dto/create-class.dto';
+import { UpdateClassDto } from './dto/update-class.dto';
 
 @Injectable()
 export class ClassService {
   constructor(private readonly prisma: DatabaseService) {}
+
+  create(data: CreateClassDto) {
+    return this.prisma.class.create({ data });
+  }
+
+  update(data: UpdateClassDto, id: number) {
+    return this.prisma.class.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.class.delete({ where: { id } });
+  }
+
+  findAll() {
+    return this.prisma.class.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.class.findFirst({ where: { id } });
+  }
 }
