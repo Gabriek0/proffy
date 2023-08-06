d50a1333550d0c847e5e08a2b47acf656b45bd68
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-06 10:17:10 -0300

    fix: change border from 2px to 1px in input

--- packages/frontend/src/components/Input/InputRoot.tsx
+++ packages/frontend/src/components/Input/InputRoot.tsx
@@ -8,10 +8,10 @@ interface InputProps {
 export function InputRoot({ children, variantStyle }: InputProps) {
   return (
     <div
-      className={`relative z-0 h-fit w-fit bg-gray-10 border-x-[2px] border-b-[2px] border-solid border-gray-30 group ${
+      className={`relative z-0 h-fit w-fit bg-gray-10 border-x-[1px] border-b-[1px] border-solid border-gray-30 group ${
         variantStyle &&
         variantStyle === `topRounded` &&
-        `border-t-[2px] rounded-t-[8px]`
+        `border-t-[1px] rounded-t-[8px]`
       } ${
         variantStyle && variantStyle === `bottomRounded` && ` rounded-b-[8px]`
       }`}
