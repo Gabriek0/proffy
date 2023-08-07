ed1b6c8b62677db64377b0f882bcdf9fd379d1c3
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-07 19:42:43 -0300

    feat: create input value state to apply label styles

--- packages/frontend/src/components/Input/InputContent.tsx
+++ packages/frontend/src/components/Input/InputContent.tsx
@@ -1,4 +1,6 @@
-import { InputHTMLAttributes } from 'react';
+'use client';
+
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -6,18 +8,28 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export function InputContent({ name, label, ...rest }: InputProps) {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value);
+  }
+
   return (
     <>
       <hr className="absolute group-focus-within:left-[-1px] group-focus-within:h-[48px] group-focus-within:w-[2px] group-focus-within:rounded-[8px] group-focus-within:bg-purple-300 group-focus-within:top-[.85rem]" />
 
       <label
         htmlFor={name}
-        className="absolute text-gray-40 text-[18px] leading[24px] font-normal top-[24px] bottom-0 mx-auto left-[24px] group-focus-within:top-[8px] group-focus-within:text-[14px] group-focus-within:text-gray-50"
+        className={`absolute text-gray-40 text-[18px] leading[24px] font-normal top-[24px] bottom-0 mx-auto left-[24px] group-focus-within:top-[8px] group-focus-within:text-[14px] group-focus-within:text-gray-50 ${
+          inputValue && 'top-[8px] text-[14px] text-gray-50'
+        }`}
       >
         {label}
       </label>
       <input
         name={name}
+        value={inputValue}
+        onChange={handleChange}
         className="h-[4.5rem] w-[22rem] min-w-[280px] font-poppins px-[24px] mt-[2px] bg-transparent outline-none  text-purple-500 font-medium text-[16px] sm:w-full"
         {...rest}
       />
