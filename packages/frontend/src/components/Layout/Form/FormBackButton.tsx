66803e1facdf84b3a3a7b332a3b9af843de82f16
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-07 20:53:58 -0300

    feat: add router.back to FormBackButton

--- packages/frontend/src/components/Layout/Form/FormBackButton.tsx
+++ packages/frontend/src/components/Layout/Form/FormBackButton.tsx
@@ -1,13 +1,20 @@
+import { useRouter } from 'next/navigation';
 import { BackButton } from 'packages/frontend/src/icons';
 import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export function FormBackButton({ ...rest }: ButtonProps) {
+  const router = useRouter();
+
+  const onBack = () => router.back();
+
   return (
     <button
-      {...rest}
+      type="button"
+      onClick={onBack}
       className="absolute top-0 bg-transparent outline-non ease-in transition hover:scale-105"
+      {...rest}
     >
       <BackButton />
     </button>
