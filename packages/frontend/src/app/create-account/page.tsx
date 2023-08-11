e99e64a9df4b71eb18cca88a195ea3d411d891e5
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-11 10:48:56 -0300

    chore: install @hookform/resolvers

--- packages/frontend/src/app/create-account/page.tsx
+++ packages/frontend/src/app/create-account/page.tsx
@@ -2,14 +2,19 @@
 
 import { Eye, EyeSlash } from 'phosphor-react';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Form } from '../../components/Layout/Form';
 import { Section } from '../../components/Layout/Section';
 
 export default function CreateAccountPage() {
+  const { handleSubmit } = useForm({
+    // resolver: zodResolver(createAccountSchema),
+  });
   const [isShowPassword, setIsShowPassword] = useState(false);
 
+  const onSubmit = () => {};
   const handleShowPassword = () => setIsShowPassword((prev) => !prev);
 
   return (
