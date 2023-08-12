92491f9ea8a28c08f74d28ca153e02edfc2b599e
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-12 18:53:53 -0300

    feat: define fix width to FormError

--- packages/frontend/src/components/Layout/Form/FormError.tsx
+++ packages/frontend/src/components/Layout/Form/FormError.tsx
@@ -5,7 +5,7 @@ interface FormProps {
 }
 export function FormError({ children }: FormProps) {
   return (
-    <span className="text-red text-[1.05rem] text-left font-medium mt-[.5rem] ml-[.25rem]">
+    <span className="text-red text-[1.05rem] text-center font-medium mt-[.5rem] ml-[.25rem] w-[22rem] sm:w-[280px]">
       {children}
     </span>
   );
