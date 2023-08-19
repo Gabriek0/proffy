72212f519b8bc62d8f9611ea30a57a8a9582ce6f
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2023-08-19 10:40:08 -0300

    feat(back): add create class endpoint

--- packages/backend/src/classes/class.controller.ts
+++ packages/backend/src/classes/class.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ClassService } from './class.service';
+import { CreateClassDto } from './dto/create-class.dto';
 
+@ApiTags('Class')
 @Controller('class')
 export class ClassController {
   constructor(private readonly classService: ClassService) {}
 
   @Post()
-  create() {
-    return 'ok';
+  create(@Body() body: CreateClassDto) {
+    return this.classService.create(body);
   }
 }
--- packages/backend/src/classes/dto/create-class.dto.ts
+++ packages/backend/src/classes/dto/create-class.dto.ts
@@ -1,9 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Class } from '@prisma/client';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateClassDto implements Class {
   id: number;
-  subject: string;
-  cost: number;
-  ownerId: number;
   createdAt: Date;
+
+  @ApiProperty()
+  @IsString()
+  subject: string;
+
+  @ApiProperty()
+  @IsNumber()
+  cost: number;
+
+  @ApiProperty()
+  @IsNumber()
+  ownerId: number;
 }
