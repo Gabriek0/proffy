8fcb1612f1893567cb2181d12ea31da490489f3e
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-09-24 11:35:04 -0300

    feat(frontend): comment password strong zod validation

--- packages/frontend/src/schema/create-account/index.ts
+++ packages/frontend/src/schema/create-account/index.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 import { convertToCapitalize } from '@front/utils/convert-to-capitalize';
-import { checkIfPasswordIsStrong } from '@front/utils/check-if-password-is-strong';
+// import { checkIfPasswordIsStrong } from '@front/utils/check-if-password-is-strong';
 
 /*  const checkPasswordStrong = () => {
   const env = process.env.NODE_ENV;
@@ -35,15 +35,13 @@ export const createAccountSchema = z
       .nonempty()
       .transform((value) => convertToCapitalize(value)),
     email: z.string().nonempty().email(),
-    password: z
-      .string()
-      .nonempty('#')
-      .min(4, { message: 'A senha deve ter no mínimo 4 caracteres' })
-      .max(20, { message: 'A senha deve ter no máximo 20 caracteres' })
-      .refine((value) => checkIfPasswordIsStrong(value), {
+    password: z.string().nonempty(),
+    // .min(4, { message: 'A senha deve ter no mínimo 4 caracteres' })
+    // .max(20, { message: 'A senha deve ter no máximo 20 caracteres' })
+    /* .refine((value) => checkIfPasswordIsStrong(value), {
         message:
           'A senha deve conter caracteres especiais, letras maíusculas, minúsculas e números.',
-      }),
+      }), */
   })
   .transform(({ first_name, last_name, email, password }) => ({
     name: `${first_name} ${last_name}`,
