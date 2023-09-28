f488fc9507f0b55f16b519aaa5326fdf8861a555
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-09-28 14:20:02 -0300

    feat(front): add env variable

--- packages/frontend/src/app/create-account/page.tsx
+++ packages/frontend/src/app/create-account/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/navigation';
 import { Eye, EyeSlash, WarningCircle } from 'phosphor-react';
 import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Form } from '../../components/Layout/Form';
