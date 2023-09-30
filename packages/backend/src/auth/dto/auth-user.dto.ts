6b9bd2dd4d2bde0ad41c05632120d98c1ecd7e07
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-09-30 18:41:05 -0300

    feat(back): create AuthUserDTOwith class validator and swagger

---
+++ packages/backend/src/auth/dto/auth-user.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
+import { AuthUser } from '../entities/auth-user.entity';
+
+class AuthUserDTO implements AuthUser {
+  @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'example@example.com',
+    required: true,
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: 'Password@_exampl3',
+    minimum: 4,
+    maximum: 20,
+    required: true,
+  })
+  password: string;
+}
