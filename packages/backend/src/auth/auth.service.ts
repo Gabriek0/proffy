417470f4fd4a8b3f098888cd15fef3e36823ff23
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-10-01 13:56:51 -0300

    feat(back): configure JwtModule

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 import { UserService } from '../user/user.service';
 import { AuthUserDTO } from './dto/auth-user.dto';
 
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  accessToken: string;
+}
+
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {}
 
-  async signIn({ email, password }: AuthUserDTO): Promise<any> {
+  async signIn({ email, password }: AuthUserDTO): Promise<IResponse> {
     const user = await this.userService.find(email);
 
     if (!user) throw new Error('User not found');
@@ -16,6 +28,15 @@ export class AuthService {
 
     if (!isValidPassword) throw new UnauthorizedException();
 
-    return { email, password };
+    const payload = { sub: user.id, name: user.name };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      accessToken,
+    };
   }
 }
