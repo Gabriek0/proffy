d474e2eb641995ba1b02a811d1c0b15c983868c8
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-10-02 13:20:29 -0300

    feat(back): add auth guard and is-public decorator

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [
@@ -11,11 +13,11 @@ import { AuthService } from './auth.service';
       global: true,
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: '60s',
+        expiresIn: '30d',
       },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AuthModule {}
